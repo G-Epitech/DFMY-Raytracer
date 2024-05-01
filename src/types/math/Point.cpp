253b16e0850e5f424ea87c6e30b072e6e9568dc6
Flavien Chenu <flavienchenu@hotmail.com>
2024-05-01 23:21:45 +0200

    test(point): add set of tests for point type

--- src/types/math/Point.cpp
+++ src/types/math/Point.cpp
@@ -16,3 +16,5 @@ std::ostream &operator<<(std::ostream &os, const Math::Point3D &point)
 Math::Point3D::Point3D(float x, float y, float z) : Float3(x, y, z) {}
 
 Math::Point3D::Point3D(const Math::Point3D &point): Point3D(point.x, point.y, point.z) {}
+
+
