464987b1f5b4217415e8de8420998164b8efceee
Yann <yannmasson17@gmail.com>
2024-05-03 13:09:25 +0200

    refactor(types): remove useless Manifest and Author type constructor

--- common/types/object/Manifest.hpp
+++ common/types/object/Manifest.hpp
@@ -13,10 +13,6 @@
 namespace Raytracer::Common::Object {
     /// @brief Author of the object
     typedef struct Author {
-        /// @brief Constructor
-        Author();
-        /// @brief Create a new author
-        Author(const std::string &name, const std::string &email, const std::string &website);
         /// @brief Name of the author
         const std::string name;
         /// @brief Public contact email
@@ -27,10 +23,6 @@ namespace Raytracer::Common::Object {
 
     /// @brief Manifest of the object
     typedef struct Manifest {
-        /// @brief Constructor
-        Manifest();
-        /// @brief Create a new manifest
-        Manifest(const std::string &name, const std::string &description, const std::string &version, const std::vector<Author> &authors);
         /// @brief Name of the object
         const std::string name;
         /// @brief Description of the object
