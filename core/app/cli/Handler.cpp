6eaefd358d016ba1a4b35acc88114f2bf3e97893
Matheo Coquet <matheo.coquet@epitech.eu>
2024-05-07 19:53:35 +0200

    refactor: add test scene

--- core/app/cli/Handler.cpp
+++ core/app/cli/Handler.cpp
@@ -24,29 +24,50 @@ Handler::~Handler() = default;
 int Handler::run() {
     DLLoader dlloader("plugins/raytracer_sphere.dylib");
 
-    std::shared_ptr<Common::Graphics::Material> material = std::make_shared<Common::Graphics::Material>(
+    std::shared_ptr<Common::Graphics::Material> green = std::make_shared<Common::Graphics::Material>(
+            Common::Graphics::Color(0, 255, 0),
+            Common::Graphics::Color(255, 255, 255),
+            Common::Graphics::Color(255, 255, 255),
+            0
+    );
+    std::shared_ptr<Common::Graphics::Material> red = std::make_shared<Common::Graphics::Material>(
             Common::Graphics::Color(255, 0, 0),
             Common::Graphics::Color(255, 255, 255),
             Common::Graphics::Color(255, 255, 255),
-            0.5f
+            0
+    );
+    std::shared_ptr<Common::Graphics::Material> purple = std::make_shared<Common::Graphics::Material>(
+            Common::Graphics::Color(0, 0, 255),
+            Common::Graphics::Color(255, 255, 255),
+            Common::Graphics::Color(255, 255, 255),
+            0
+    );
+    std::shared_ptr<Common::Graphics::Material> light = std::make_shared<Common::Graphics::Material>(
+            Common::Graphics::Color(255, 255, 255),
+            Common::Graphics::Color(255, 255, 255),
+            Common::Graphics::Color(255, 255, 255),
+            100.0f
     );
 
     auto objectProvider = dlloader.loadSymbol<Common::ObjectProviderGetter>(SHARED_STRINGIFY(OBJECT_PROVIDER_GETTER_NAME));
-    auto sphere = objectProvider()->create(material, Common::Math::Point3D(0, 30, 30), 10.0f);
 
     Rendering::Camera::Config camConfig = {
             .name = "Main camera",
             .screen = {
-                    .size = { .width = 1920, .height = 1080 }
+                    .size = { .width = 1920 * 2, .height = 1080 * 2 }
             },
             .position = Common::Math::Point3D(0, 0, 30),
             .direction = Common::Math::Vector3D(0, 0, 0),
-            .fov = 150
+            .fov = 50
     };
     Rendering::Camera camera(camConfig);
     std::vector<Common::IObject::Ptr> objects;
 
-    objects.push_back(sphere);
+    objects.push_back(objectProvider()->create(light, Common::Math::Point3D(-35, 100, 80), 50.0f));
+    objects.push_back(objectProvider()->create(purple, Common::Math::Point3D(0, 80, -75), 100.0f));
+    objects.push_back(objectProvider()->create(red, Common::Math::Point3D(-10, 60, 30), 10.0f));
+    objects.push_back(objectProvider()->create(green, Common::Math::Point3D(10, 75, 35), 13.0f));
+
     camera.compute(COMPUTE_THREADS, objects);
 
     for (auto& thread : camera._threads) {
