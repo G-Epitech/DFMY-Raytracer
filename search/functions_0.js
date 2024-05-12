080754f857b0456ba5af2c6dd57676a83da4cccd
TekMath <58297773+TekMath@users.noreply.github.com>
2024-05-12 15:52:43 +0000

    Deploying to gh-pages from @ G-Epitech/DFMY-Raytracer@559ddc0753f50fe918bee67cdf0c1ab2f2d0f025 🚀

--- search/functions_0.js
+++ search/functions_0.js
@@ -1,8 +1,8 @@
 var searchData=
 [
-  ['_5fcomputeframe_332',['_computeFrame',['../classRaytracer_1_1Core_1_1Rendering_1_1Camera.html#af1fa67079d2af56609f4f4dfd43d293b',1,'Raytracer::Core::Rendering::Camera']]],
+  ['_5fcomputeframe_332',['_computeFrame',['../classRaytracer_1_1Core_1_1Rendering_1_1Camera.html#aa90e44d36976830333f8aa73f9d16d3c',1,'Raytracer::Core::Rendering::Camera']]],
   ['_5fcomputeraycollision_333',['_computeRayCollision',['../classRaytracer_1_1Core_1_1Rendering_1_1Camera.html#a33b6086f42c8d272a2145de5f835e055',1,'Raytracer::Core::Rendering::Camera']]],
-  ['_5fcomputesegment_334',['_computeSegment',['../classRaytracer_1_1Core_1_1Rendering_1_1Camera.html#a316aeb1dc70893720e17291c84fee7ad',1,'Raytracer::Core::Rendering::Camera']]],
-  ['_5fgetincominglight_335',['_getIncomingLight',['../classRaytracer_1_1Core_1_1Rendering_1_1Camera.html#a5049a0e927834a605e940b8a5cefc3d5',1,'Raytracer::Core::Rendering::Camera']]],
+  ['_5fcomputesegment_334',['_computeSegment',['../classRaytracer_1_1Core_1_1Rendering_1_1Camera.html#a03167d7a69faca43dc26d504d42499d5',1,'Raytracer::Core::Rendering::Camera']]],
+  ['_5fgetincominglight_335',['_getIncomingLight',['../classRaytracer_1_1Core_1_1Rendering_1_1Camera.html#a6f4ccfe8e8372f79f9709da7a4b7dfbb',1,'Raytracer::Core::Rendering::Camera']]],
   ['_5fgetrandomdirection_336',['_getRandomDirection',['../classRaytracer_1_1Core_1_1Rendering_1_1Camera.html#a8463346e46430a0fa7a4a460eb2f42c8',1,'Raytracer::Core::Rendering::Camera']]]
 ];
