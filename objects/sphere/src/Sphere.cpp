3ce7dff9126578aca675c06514fce18961411d61
Matheo Coquet <matheo.coquet@epitech.eu>
2024-05-12 18:41:35 +0200

    feat: add directional light

--- objects/sphere/src/Sphere.cpp
+++ objects/sphere/src/Sphere.cpp
@@ -46,14 +46,24 @@ Raytracer::Common::Math::HitInfo Raytracer::Objects::Sphere::computeCollision(co
     hitInfo.distance = distance;
     auto intersectionNormal = ray.direction * distance;
     Common::Math::Point3D hitPointData(intersectionNormal.x, intersectionNormal.y, intersectionNormal.z);
+
     hitInfo.hitPoint = ray.origin + hitPointData;
     hitInfo.normal = (hitInfo.hitPoint - _position).normalize();
+
     hitInfo.hitColor = {
-        .color = _material->color,
-        .emissionStrength = _material->emissionStrength,
-        .emissionColor = _material->emissionColor,
-        .reflectivity = _material->reflectivity,
+            .color = _material->color,
+            .emissionStrength = _material->emissionStrength,
+            .emissionColor = _material->emissionColor,
+            .reflectivity = _material->reflectivity,
     };
+
+    for (auto &emission : _material->emissions) {
+        auto angle = std::acosf(emission.direction.normalize().dot(hitInfo.normal));
+        if (angle < M_PI_2) {
+            hitInfo.hitColor.emissionColor = emission.color;
+            hitInfo.hitColor.emissionStrength = emission.strength;
+        }
+    }
     return hitInfo;
 }
 
